9ca2950240d5ccc547a64ae5a3d01c61744958e0
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-11-01 09:05:40 +0200

    chore: remove 'nomad-xyz' and 'thirdweb-contracts' libs

--- test/mocks/MockNonCompliantSpace.sol
+++ test/mocks/MockNonCompliantSpace.sol
@@ -1,14 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.26;
 
-import { ExcessivelySafeCall } from "@nomad-xyz/excessively-safe-call/src/ExcessivelySafeCall.sol";
 import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 /// @title MockNonCompliantSpace
 /// @notice A mock non-compliant {Space} contract that do not support the {ISpace} interface
 contract MockNonCompliantSpace is IERC165 {
-    using ExcessivelySafeCall for address;
-
     address public owner;
 
     event ModuleExecutionSucceded(address module, uint256 value, bytes data);
@@ -22,29 +19,30 @@ contract MockNonCompliantSpace is IERC165 {
         _;
     }
 
-    function execute(address module, uint256 value, bytes memory data) external onlyOwner returns (bool success) {
-        // Allocate all the gas to the executed module method
-        uint256 txGas = gasleft();
-
-        // Execute the call via assembly and get only the first 4 bytes of the returndata
-        // which will be the selector of the error in case of a revert in the module contract
-        // See https://github.com/nomad-xyz/ExcessivelySafeCall
-        bytes memory result;
-        (success, result) = module.excessivelySafeCall({ _gas: txGas, _value: 0, _maxCopy: 4, _calldata: data });
-
-        if (!success) {
-            // Revert with the same error returned by the module contract
-            assembly {
-                revert(add(result, 0x20), 4)
-            }
-            // Log the execution success
-        } else {
-            emit ModuleExecutionSucceded(module, value, data);
-        }
+    function execute(address module, uint256 value, bytes calldata data) public returns (bool success) {
+        // Effects, Interactions: execute the call on the `module` contract
+        success = _call(module, value, data);
     }
 
     /// @inheritdoc IERC165
     function supportsInterface(bytes4 interfaceId) public pure override returns (bool) {
         return interfaceId == type(IERC165).interfaceId;
+    }
+
+    /// @dev Executes a low-level call on the `module` contract with the `data` data forwarding the `value` value
+    function _call(address module, uint256 value, bytes calldata data) internal returns (bool success) {
+        // Execute the call via assembly
+        bytes memory result;
+        (success, result) = module.call{ value: value }(data);
+
+        // Revert with the same error returned by the module contract if the call failed
+        if (!success) {
+            assembly {
+                revert(add(result, 0x20), mload(result))
+            }
+        } else {
+            // Otherwise log the execution success
+            emit ModuleExecutionSucceded(module, value, data);
+        }
     }
 }
