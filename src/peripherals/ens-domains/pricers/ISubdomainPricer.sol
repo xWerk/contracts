45462c817169ec39bdd171cf3816e8f1df646f01
gabrielstoica <marius_gabriel1998@yahoo.com>
2025-01-09 16:36:16 +0200

    feat: add 'ForeverSubdomainRegistrar.sol' base implementation

---
+++ src/peripherals/ens-domains/pricers/ISubdomainPricer.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+interface ISubdomainPricer {
+    function price(
+        bytes32 parentNode,
+        string calldata label,
+        uint256 duration
+    )
+        external
+        view
+        returns (address token, uint256 price);
+}
