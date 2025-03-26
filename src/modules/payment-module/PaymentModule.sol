aa61acd6e7a0af09dc81d036c676e7d17d082ca1
gabrielstoica <marius_gabriel1998@yahoo.com>
2025-03-26 16:27:53 +0200

    fix(payment-module): remove redundant request status in 'payRequest'

--- src/modules/payment-module/PaymentModule.sol
+++ src/modules/payment-module/PaymentModule.sol
@@ -238,7 +238,7 @@ contract PaymentModule is IPaymentModule, StreamManager, UUPSUpgradeable {
 
         // Checks: the payment request is not already paid or canceled
         // Note: for stream-based requests the `status` changes to `Paid` only after the funds are fully streamed
-        if (requestStatus == Types.Status.Paid || request.config.paymentsLeft == 0) {
+        if (requestStatus == Types.Status.Paid) {
             revert Errors.RequestPaid();
         } else if (requestStatus == Types.Status.Canceled) {
             revert Errors.RequestCanceled();
