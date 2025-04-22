8a2ad5bd7cc04a6787856afa3229ee61324c7703
gabrielstoica <marius_gabriel1998@yahoo.com>
2025-04-22 15:34:58 +0300

    feat: define 'Compensation' types and errors

---
+++ src/modules/compensation-module/libraries/Errors.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.26;
+
+/// @title Errors
+/// @notice Library containing all custom errors the {CompensationModule} contract may revert with
+library Errors {
+    /*//////////////////////////////////////////////////////////////////////////
+                                    COMPENSATION-MODULE
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice Thrown when the caller is an invalid zero code contract or EOA
+    error SpaceZeroCodeSize();
+
+    /// @notice Thrown when the recipient address is the zero address
+    error InvalidZeroAddressRecipient();
+
+    /// @notice Thrown when the caller is a contract that does not implement the {ISpace} interface
+    error SpaceUnsupportedInterface();
+}
--- src/modules/compensation-module/libraries/Types.sol
+++ src/modules/compensation-module/libraries/Types.sol
@@ -1,5 +1,43 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.26;
 
+import { UD21x18 } from "@prb/math/src/UD21x18.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 /// @notice Namespace for the structs used across the {CompensationModule} related contracts
-library Types { }
+library Types {
+    /// @notice Struct encapsulating the different values describing a compensation plan
+    /// @param recipient The address of the recipient of the compensation
+    /// @param packages The packages included in the compensation (salary, ESOPs, bonuses, etc.)
+    struct Compensation {
+        address recipient;
+        Package[] packages;
+    }
+
+    /// @notice Struct encapsulating the different values describing a package within a compensation plan
+    /// @param packageType The type of compensation package
+    /// @param asset The address of the compensation asset
+    /// @param ratePerSecond The rate per second of the compensation package
+    /// @param amount The amount of compensation
+    /// @param streamId The ID of the stream used to pay the compensation package
+    struct Package {
+        // slot 0
+        PackageType packageType; // 1 byte
+        IERC20 asset; // 20 bytes
+        // slot 1
+        UD21x18 ratePerSecond; // 16 bytes
+        uint128 amount; // 16 bytes
+        // slot 2
+        uint256 streamId; // 32 bytes
+    }
+
+    /// @notice Enum representing the different types of a compensation package
+    /// @custom:value Payroll Compensation package for a payroll
+    /// @custom:value Payout Compensation package for a payout
+    /// @custom:value ESOP Compensation package for a ESOP
+    enum PackageType {
+        Payroll,
+        Payout,
+        ESOP
+    }
+}
